30e2b976dc98dc9326e7e7e514be68f50d309d8f
Ethiraric <ethiraric@gmail.com>
2021-06-14 20:28:35 +0200

    Minor fixes in polybar.

--- dotfiles/config/polybar/launch.sh
+++ dotfiles/config/polybar/launch.sh
@@ -9,7 +9,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 {%@@ if profile == "Tswarra" @@%}
 
-MONITOR=$MAIN_SCREEN polybar main_top -c ~/.config/polybar/config.ini &
+# MONITOR=$MAIN_SCREEN polybar main_top -c ~/.config/polybar/config.ini &
 MONITOR=$MAIN_SCREEN polybar main_bot -l info -c ~/.config/polybar/config.ini &
 
 # MONITOR=$SECONDARY_SCREEN polybar secondary_top -c ~/.config/polybar/config.ini &
