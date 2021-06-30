59ab9211071e8a534346572ae694819decb08907
Ethiraric <ethiraric@gmail.com>
2021-06-30 23:22:33 +0200

    Download packer if not installed.

--- dotfiles/config/nvim/lua/plugins.lua
+++ dotfiles/config/nvim/lua/plugins.lua
@@ -1,5 +1,15 @@
 -- {{@@ header() @@}}
 
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', '--depth', '1', install_path})
+  execute 'packadd packer.nvim'
+end
+
 return require('packer').startup(function (use)
   ----------  Package manager
   use 'wbthomason/packer.nvim'
