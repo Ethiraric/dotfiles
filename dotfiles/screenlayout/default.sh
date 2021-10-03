6d3df1ecbdc1e4e1ebaffc0b2176b5008efa4a38
Ethiraric <ethiraric@gmail.com>
2021-10-03 14:02:14 +0200

    Update screenlayout.

--- dotfiles/screenlayout/default.sh
+++ dotfiles/screenlayout/default.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-xrandr --output HDMI-0 --off --output DP-0 --mode 1920x1200 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --mode 1920x1200 --pos 1920x0 --rotate normal --output DP-3 --off --output DP-4 --off --output DP-5 --off
+
+# {{@@ header() @@}}
+
+{%@@ if profile == "Jotho" @@%}
+xrandr --output HDMI-0 --off --output DP-0 --mode 1920x1080 --pos 0x60 --rate 140.00 --rotate normal --output DP-1 --off --output DP-2 --mode 1920x1200 --pos 1920x0 --rotate normal --output DP-3 --off --output DP-4 --off --output DP-5 --off
+{%@@ endif @@%}
