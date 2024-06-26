54c942de309e8d35168a98086b7cf213baccf7ff
Ethiraric <ethiraric@gmail.com>
2024-06-26 18:36:18 +0200

    Update LspTrouble config.

--- dotfiles/config/nvim/lua/plugin-cfg/trouble.lua
+++ dotfiles/config/nvim/lua/plugin-cfg/trouble.lua
@@ -1,6 +1,6 @@
 -- {{@@ header() @@}}
 
-return function ()
+return function()
   local map = require('utils').map
 
   require('trouble').setup {
@@ -8,6 +8,7 @@ return function ()
     use_diagnostic_signs = true,
   }
 
-  map('n', '<leader>l', ':TroubleToggle<CR>')
-  map('n', '<leader>L', ':TroubleToggle workspace_diagnostics<CR>')
+  map('n', '<leader>l', ':Trouble diagnostics toggle filter.buf=0<CR>')
+  map('n', '<leader>L', ':Trouble diagnostics toggle<CR>')
+  map('n', '<leader>s', ':Trouble symbols toggle pinned=true results.win.relative=win results.win.position=right<CR>')
 end
