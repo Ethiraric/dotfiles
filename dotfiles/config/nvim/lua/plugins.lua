50258aea207234b385a9026761ab5795bae222e2
Ethiraric <ethiraric@gmail.com>
2024-06-26 18:36:10 +0200

    Unlock fidget.nvim from legacy version.

--- dotfiles/config/nvim/lua/plugins.lua
+++ dotfiles/config/nvim/lua/plugins.lua
@@ -56,7 +56,6 @@ return require('packer').startup({
     }
     use {
       'j-hui/fidget.nvim', -- Display progress of LSP
-      tag = 'legacy',
       config = require('plugin-cfg.fidget')
     }
 
